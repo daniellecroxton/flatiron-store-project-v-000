5cf6a6fecca09e728a7774916e85a1eb7226f9e9
daniellecroxton <danikrist@gmail.com>
2018-04-01 22:58:19 -0600

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,20 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, through: :line_items
+  belongs_to :user
+
+  def add_item(item)
+    # binding.pry
+    if self.line_items.ids.detect(item)
+      line_item.quantity += 1
+    else
+      LineItem.new(cart_id: self.id, item_id: item, quantity: 1)
+    end
+  end
+
+  def total
+    #prices * quantities
+    #sum
+  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ActiveRecord::Base
+  belongs_to :category
+  has_many :line_items
+  has_many :carts, through: :line_items
 
+  def self.available_items
+    self.all.drop(0).collect do | i |
+      if i.inventory > 0
+      end
+    end
+  end
 end
